7e82c01f1447e6a33299fe19579aba1b87510078
Daniel Coy <danielfelipe113@gmail.com>
2020-08-16 08:50:39 -0500

    Change cheerio to puppeteer

--- build/index.js
+++ build/index.js
@@ -8,54 +8,129 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-Object.defineProperty(exports, "__esModule", { value: true });
 const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const youtubedl = require('youtube-dl');
+const sanitize = require("sanitize-filename");
+const puppeteer = require('puppeteer');
+const userAgent = require('user-agents');
 // Globals
-const baseUrl = 'https://fast.wistia.net/embed/iframe/';
+const videProviderBaseUrl = 'https://fast.wistia.net/embed/iframe/';
 const basePath = `${__dirname}/videos/`;
+const credentials = {
+    email: 'danielfelipe113@gmail.com',
+    password: 'despertarkaren333'
+};
 if (!fs.existsSync(basePath)) {
     fs.mkdirSync(basePath);
 }
-function cheerioExample() {
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    const browser = yield puppeteer.launch({ headless: false });
+    const page = yield browser.newPage();
+    yield page.setDefaultNavigationTimeout(0);
+    yield page.setUserAgent(userAgent.toString());
+    // await page.goto('https://sso.teachable.com/secure/210848/users/sign_in', {waitUntil: 'networkidle2'});
+    // await page.screenshot({path: 'example.png'});
+    // await page.type('#user_email', credentials.email);
+    // await page.type('#user_password', credentials.password);
+    // await page.keyboard.press('Enter');
+    // await page.waitForNavigation();
+    // await page.screenshot({path: 'example2.png'});
+    // console.log('New Page URL:', page.url());
+    // await page.waitForNavigation();
+    // await page.screenshot({path: 'example3.png'});
+    //In!
+    // await page.goto('https://sso.teachable.com/secure/210848/users/sign_in', {waitUntil: 'networkidle2'});
+    // 'data-course-url'
+    let coursesPages = [];
+    yield page.goto('https://pro.academind.com/courses/javascript-algorithms-the-fundamentals/lectures/16817672', { waitUntil: 'networkidle2' });
+    getVideoUrls(page);
+    // await browser.close();
+}))();
+// getVideoUrls('https://pro.academind.com', '/courses/javascript-algorithms-the-fundamentals/lectures/16817672');
+function getVideoUrls(page) {
     return __awaiter(this, void 0, void 0, function* () {
-        const pageContent = yield axios.get('https://pro.academind.com/courses/javascript-algorithms-the-fundamentals/lectures/16817672');
-        fs.writeFileSync(`${__dirname}/test.txt`, pageContent.data);
-        const $ = cheerio.load(pageContent.data);
-        let videoKeys = [];
-        const videoTitle = $("#lecture_heading").text();
-        const presentations = $(".attachment-wistia-player")
-            .map((idx, el) => {
-            el = $(el);
-            el.each((index, eachEl) => {
-                const eachElCheer = $(eachEl);
-                const key = eachElCheer.attr("data-wistia-id");
-                if (!!key) {
-                    videoKeys.push(key);
-                }
+        try {
+            console.log('Getting sections');
+            const data = yield page.evaluate(() => {
+                const sections = Array
+                    .from(document.querySelectorAll('.course-section'))
+                    .map(elem => elem.tagName);
+                console.log(sections);
             });
-        });
-        let video;
-        let validName;
-        videoKeys.forEach(videoKey => {
-            validName = getValidName(basePath, videoTitle);
-            console.log(validName);
-            return;
-            video = youtubedl(baseUrl + videoKey, 
-            // Optional arguments passed to youtube-dl.
-            [], 
-            // Additional options can be given for calling `child_process.execFile()`.
-            { cwd: __dirname });
-            video.pipe(fs.createWriteStream(`${basePath}${validName.trim()}.mp4`));
-        });
+            // $('.course-section')
+            //     .map((idx: number, el: Cheerio) => {
+            //         el = $(el);
+            //         el.each((index: number, eachEl: CheerioElement) => {
+            //             const eachElCheer: Cheerio = $(eachEl);
+            //             const anchors = eachElCheer.find('a');
+            //             console.log('Getting anchors');
+            //             anchors.each((idx, anchor) => {
+            //                 const anchorElem: Cheerio = $(anchor);
+            //                 const anchorSrc = anchorElem.attr("href"); 
+            //                 if(!!anchorSrc) {
+            //                     getVideo(baseUrl+anchorSrc, idx);
+            //                 }
+            //             })
+            //         });
+            //     });
+        }
+        catch (error) {
+            console.log('ERROR - Can\'t get page content', error);
+        }
     });
 }
-function getValidName(path = "", videoTitle = "", modificator) {
-    console.log('mpodioficator  ', modificator);
-    const fileName = `${videoTitle}${modificator || ""}`;
-    if (fs.existsSync(`${basePath}${fileName.trim()}.mp4`)) {
+function getVideo(url, counter) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let pageContent;
+        try {
+            pageContent = yield axios.get(url);
+            const $ = cheerio.load(pageContent.data);
+            const videoTitle = $("#lecture_heading").text().trim();
+            const videoKeys = getVideoKeys($);
+            let video;
+            let validName;
+            videoKeys.forEach(videoKey => {
+                validName = getValidName(basePath, counter, videoTitle);
+                console.log('Starting video download');
+                video = youtubedl(videProviderBaseUrl + videoKey, 
+                // Optional arguments passed to youtube-dl.
+                [], 
+                // Additional options can be given for calling `child_process.execFile()`.
+                { cwd: __dirname });
+                console.log('Finished video download');
+                console.log('Starting video saving');
+                video.pipe(fs.createWriteStream(`${basePath}${validName.trim()}.mp4`));
+                console.log('Video saved in ' + basePath + ' with name: ' + `${validName.trim()}.mp4`);
+            });
+        }
+        catch (error) {
+            console.log('ERROR - Can\'t get page content', error);
+        }
+    });
+}
+function getVideoKeys(cheerioInstance) {
+    const $ = cheerioInstance;
+    let videoKeys = [];
+    console.log('Getting video key');
+    $(".attachment-wistia-player")
+        .map((idx, el) => {
+        el = $(el);
+        el.each((index, eachEl) => {
+            const eachElCheer = $(eachEl);
+            const key = eachElCheer.attr("data-wistia-id");
+            if (!!key) {
+                videoKeys.push(key);
+            }
+        });
+    });
+    return videoKeys;
+}
+function getValidName(path = "", counter, videoTitle = "", modificator) {
+    console.log('Getting valid name');
+    const fileName = `${!!counter ? counter + '. ' : ''}${videoTitle}${modificator || ""}`;
+    if (fs.existsSync(`${basePath}${sanitize(fileName.trim())}.mp4`)) {
         //file exists
         if (modificator !== undefined) {
             modificator++;
@@ -64,8 +139,8 @@ function getValidName(path = "", videoTitle = "", modificator) {
             modificator = 0;
         }
         ;
-        return getValidName(path, videoTitle, modificator);
+        return getValidName(path, counter, videoTitle, modificator);
     }
-    return fileName;
+    return sanitize(fileName);
 }
-cheerioExample();
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,98 +1,186 @@
+require('dotenv').config()
 const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const youtubedl = require('youtube-dl')
 const sanitize = require("sanitize-filename");
-
+const puppeteer = require('puppeteer');
+const userAgent = require('user-agents');
 
 // Globals
 const videProviderBaseUrl: string = 'https://fast.wistia.net/embed/iframe/';
 const basePath: string = `${__dirname}/videos/`;
 
+const credentials: {
+    email: any,
+    password: any
+} = {
+    email: process.env.DB_USER,
+    password: process.env.DB_PASS
+}
+
+
 if (!fs.existsSync(basePath)){
     fs.mkdirSync(basePath);
 }
 
+function delay(time: number) {
+    return new Promise(function(resolve) { 
+        setTimeout(resolve, time)
+    });
+ }
 
 
-getVideoUrls('https://pro.academind.com', '/courses/javascript-algorithms-the-fundamentals/lectures/16817672');
+(async () => {
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(0); 
+    await page.setUserAgent(userAgent.toString())
+    await page.goto('https://sso.teachable.com/secure/210848/users/sign_in', {waitUntil: 'networkidle2'});
+    await page.screenshot({path: 'example.png'});
+    await page.type('#user_email', credentials.email);
+    await page.type('#user_password', credentials.password);
 
-async function getVideoUrls(baseUrl: string, path: string) {
-    let pageContent;
+    await page.keyboard.press('Enter');
+    
+    await page.waitForNavigation();
+    await page.screenshot({path: 'example2.png'});
+    await page.waitForNavigation();
+    await page.screenshot({path: 'example3.png'});
+    //In!
+    await page.goto('https://sso.teachable.com/secure/210848/users/sign_in', {waitUntil: 'networkidle2'});
+    
+    await page.goto('https://pro.academind.com/courses/javascript-algorithms-the-fundamentals/lectures/16817672', {waitUntil: 'networkidle2'});
+    
+    await getVideoUrls(page);
+
+    await browser.close();
+})();
+
+
+// getVideoUrls('https://pro.academind.com', '/courses/javascript-algorithms-the-fundamentals/lectures/16817672');
+
+async function getVideoUrls(page: any) {
     try {
-        pageContent = await axios.get(baseUrl + path);
-        const $ = cheerio.load(pageContent.data);
-        console.log('Getting sections');
-        $('.course-section')
-            .map((idx: number, el: Cheerio) => {
-                el = $(el);
-                el.each((index: number, eachEl: CheerioElement) => {
-                    const eachElCheer: Cheerio = $(eachEl);
-                    const anchors = eachElCheer.find('a');
-                    console.log('Getting anchors');
-                    anchors.each((idx, anchor) => {
-                        const anchorElem: Cheerio = $(anchor);
-                        const anchorSrc = anchorElem.attr("href"); 
-                        if(!!anchorSrc) {
-                            getVideo(baseUrl+anchorSrc, idx);
+        console.log('Getting sections');     
+        
+        const anchors = await page.evaluate(
+            (sel: any) => {
+                let sections = Array.from(document.querySelectorAll(sel));
+                let hrefs = [];
+                let title;
+                let toReturn: any = [];
+
+                for (const section of sections) {
+                    hrefs = [];
+                    title = section.getElementsByClassName('section-title')[0].textContent;
+                    const anchors: any = Array.from(section.getElementsByTagName('a'));
+                    for (const href of anchors) {
+                        if(!!href) {
+                            hrefs.push(href.href);
                         }
-                    })
+                    }
+                    toReturn.push({
+                        title,
+                        hrefs
+                    });
+                }                
+                return toReturn;
+            }, '.course-section');
+        
+        let sections: any = [];
+        anchors.forEach((anchor: any, idx: number) => {
+            if(anchor.hrefs.length) {
+                sections.push({
+                    title: idx + '. ' + sanitize(anchor.title).trim(),
+                    hrefs: anchor.hrefs
                 });
-            });
-    } catch(error) {
+            }
+        });
+        // Visit each assets page one by one
+        for (let section in sections) {
+            console.log('Going to ', sections[section].title);
+            
+            if (!fs.existsSync(basePath + sections[section].title)){
+                fs.mkdirSync(basePath + sections[section].title);
+            }
+            let i = 0;
+            for (let idx in sections[section].hrefs) {
+                
+                await page.goto(sections[section].hrefs[idx], {waitUntil: 'networkidle0'});
+    
+                const videoTitle = await page.evaluate((elem: any) => {
+                    const elements = Array.from(document.querySelectorAll(elem));
+                    return elements[0].textContent
+                }, '#lecture_heading');
+
+                try {
+                    console.log('Getting video keys');
+                    
+                    const videoKeys = await page.evaluate(
+                        (sel: any) => {
+                            let sections = Array.from(document.querySelectorAll(sel));
+                            let keys = [];
+                            let key;
+
+                            for (const section of sections) {
+                                key = section.getAttribute('data-wistia-id');
+                                if(!!key) {
+                                    keys.push(key);
+                                }
+                            }                
+                            return keys;
+                        }, '.attachment-wistia-player');
+
+                    let video: any;
+                    let validName: string;
+
+                    for(let key of videoKeys) {                         
+                        validName = getValidName(basePath + sections[section].title + '/', i, videoTitle);
+                        console.log('Starting video download');
+                        video = youtubedl(videProviderBaseUrl + key,
+                            // Optional arguments passed to youtube-dl.
+                            [],
+                            // Additional options can be given for calling `child_process.execFile()`.
+                            { cwd: __dirname });
+                        // Will be called when the download starts.
+                        video.on('info', function(info: any) {
+                            console.log('Download started')
+                            console.log('filename: ' + info._filename)
+                            console.log('size: ' + info.size)
+                        });
+                        video.on('error', function error(err: any) {
+                            console.log('Video download error:', err)
+                        });
+                        console.log('Finished video download');
+                        console.log('Starting video saving');
+                        
+                        await new Promise<void>(resolve => {
+                            const b = fs.createWriteStream(`${basePath + sections[section].title + '/'}${validName.trim()}.mp4`);
+                            video.pipe(b);
+                            b.on('finish', resolve);
+                        });
+                        console.log('Video saved in ' + basePath + sections[section].title + '/' + ' with name: ' + `${validName.trim()}.mp4`);
+                        i++
+                    }   
+                } catch(error) {
+                    console.log('ERROR - Can\'t get page content', error);
+                }   
+            }
+        }
         
+        console.log('Everything is now in your pc, use this script with caution and content permission');
+
+       
+    } catch(error) {
+        console.log('ERROR - Can\'t get page content', error);
     }    
 }
 
-async function getVideo(url: string, counter?: string |number) {
-    let pageContent;
-    try {
-        pageContent = await axios.get(url);
-        const $ = cheerio.load(pageContent.data);
-        const videoTitle = $("#lecture_heading").text().trim();
-        const videoKeys = getVideoKeys($);    
-        let video;
-        let validName: string;
-        videoKeys.forEach(videoKey => {
-            validName = getValidName(basePath, counter, videoTitle,);
-            console.log('Starting video download');
-            video = youtubedl(videProviderBaseUrl + videoKey,
-                // Optional arguments passed to youtube-dl.
-                [],
-                // Additional options can be given for calling `child_process.execFile()`.
-                { cwd: __dirname })
-            console.log('Finished video download');
-            console.log('Starting video saving');
-            video.pipe(fs.createWriteStream(`${basePath}${validName.trim()}.mp4`));
-            console.log('Video saved in ' + basePath + ' with name: ' + `${validName.trim()}.mp4`);
-        });
-    } catch(error) {
-        
-    }    
-}
-
-function getVideoKeys(cheerioInstance: any) {
-    const $ = cheerioInstance;
-    let videoKeys: string[] = [];
-    console.log('Getting video key');
-    $(".attachment-wistia-player")
-        .map((idx: number, el: Cheerio) => {            
-            el = $(el);
-            el.each((index: number, eachEl: CheerioElement) => {
-                const eachElCheer: Cheerio = $(eachEl);
-                const key = eachElCheer.attr("data-wistia-id");
-                if(!!key) {
-                    videoKeys.push(key);
-                }
-            });
-        });
-    return videoKeys;
-}
-
-function getValidName(path: string="", counter?: string | number, videoTitle: string="", modificator?: number): string {
+function getValidName(path: string="", counter: string | number, videoTitle: string="", modificator?: number): string {
     console.log('Getting valid name');
-    const fileName: string = `${!!counter ? counter + '. ' : ''}${videoTitle}${modificator || ""}`;
-    if (fs.existsSync(`${basePath}${sanitize(fileName.trim())}.mp4`)) {
+    const fileName: string = `${!!counter.toString() ? counter + '. ' : ''}${videoTitle.trim()}${modificator || ""}`;
+    if (fs.existsSync(`${basePath}${sanitize(fileName)}.mp4`)) {
         //file exists
         if(modificator !== undefined) {
             modificator++;
